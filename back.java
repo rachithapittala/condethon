aac98a8878872b65862966d260dc3ba366334f7c
rachithapittala <44570868+rachithapittala@users.noreply.github.com>
2018-10-29 17:24:41 +0530

    Add files via upload

---
+++ back.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.*;
+import javax.http.*;
+class back{
+ public static void main(String args[])
+{     back obj=new back();
+File *fp;
+     int p[100];
+if(action.get()==1)
+  obj.display(f1.txt);
+else if(action.get()==2)
+obj.display(f2.txt);
+
+}
+void display(file *p)
+{
+file *f;
+f=fopen(p,"r");
+while(f!='\0')
+{
+fputc("%s",*f);
+*f++:
+}
+}
+}
+
